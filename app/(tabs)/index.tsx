a45d9c80844744b6d50d53dddc55ff836e2797c5
owner <owner@Never-Stops-MacBook-Pro.local>
2026-06-07 13:56:16 -0400

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,50 +1,35 @@
 import "@/global.css";
 import { Link } from "expo-router";
 import { styled } from "nativewind";
-import { Pressable, Text, View } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <View className="w-full max-w-sm self-center">
-        <Text className="text-xl font-bold text-success">
-          Welcome to Nativewind!
-        </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
 
-        <Link href="/onboarding" asChild>
-          <Pressable className="mt-4 rounded bg-primary p-4">
-            <Text className="text-white">Go to Onboarding</Text>
-          </Pressable>
-        </Link>
+      <Link
+        href="/onboarding"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
+      >
+        Go to Onboarding
+      </Link>
 
-        <Link href="/(auth)/sign-in" asChild>
-          <Pressable className="mt-4 rounded bg-primary p-4">
-            <Text className="text-white">Go to Sign In</Text>
-          </Pressable>
-        </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
+      >
+        Go to Sign in
+      </Link>
 
-        <Link href="/(auth)/sign-up" asChild>
-          <Pressable className="mt-4 rounded bg-primary p-4">
-            <Text className="text-white">Go to Sign Up</Text>
-          </Pressable>
-        </Link>
-
-        <Link href="/subscriptions/spotify" className="mt-2">
-          Spotify Subscription
-        </Link>
-
-        <Link
-          href={{
-            pathname: "/subscriptions/[id]",
-            params: { id: "claude" },
-          }}
-        >
-          Claude Max Subscription
-        </Link>
-      </View>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 font-sans-bold rounded bg-primary p-4 text-white"
+      >
+        Go to Sign up
+      </Link>
     </SafeAreaView>
   );
 }

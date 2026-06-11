8a1ff5b0acb38d03c6be951c9216ca5bbb5b9753
owner <owner@Never-Stops-MacBook-Pro.local>
2026-06-11 08:00:12 -0400

    implement the home page

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+import avatar from "../assets/images/avatar.png";
+import splashPattern from "../assets/images/splash-pattern.png";
+
+const images = {
+  avatar,
+  splashPattern,
+};
+
+export default images;
